0773b7f3f7ac18efd1576ef4ba750e2edfb870d8
Rufus Wong <rcywongaa@gmail.com>
2025-01-10 15:18:14 -0500

    rover : Create rover action server

--- ros2_ws/src/rover/README.md
+++ ros2_ws/src/rover/README.md
@@ -1,5 +1,38 @@
 # Rover
 
+## FSM Dreamlist
+<https://github.com/erikzenker/hsm>
+<https://smacc.dev/statechart-vs-msm/>
+
+Must haves:
+- Superstate enter/exit actions
+- Direct transitions to/from substates (which should trigger the appropriate superstate enter/exit actions)
+- Events sent to both super state and leaf state (super state overrides leaf state transitions)
+Good to haves:
+- Per state member variables
+- Per state transition tables
+
+## FSM Decision
+- ~~TinyFSM~~: No support for hierarchical FSM
+- ~~SML~~: Hacky solutions for direct transitions to/from substates
+- ~~SMACC2~~: [Only leaf states can contain orthogonals](https://smacc.dev/hierarchical-states/) means superstates cannot have enter/exit actions.
+- ~~HFSM2~~: Superstates don't receive events
+- ~~MSM~~: Doesn't scale to large state machines, also same problem as SML
+- Statecharts
+
+## States
+- Autonomous
+  - On nav target:
+    - Transition to Manual
+  - Working
+  - Charging
+- Manual
+  - On Enter: Start Timer
+  - On timer expire:
+    - Transition to autonomous
+  - Substates:
+    - Go to target
+- Sleep
 ## Useful Commands
 ```
 ros2 service call /send_nav_target rover_interfaces/srv/SendNavTarget '{"target": {"position": {"latitude": 50, "longitude": 50, "altitude": 0}, "heading": 0}}'
--- ros2_ws/src/rover/src/main.cpp
+++ ros2_ws/src/rover/src/main.cpp
@@ -2,13 +2,16 @@
 #include <memory>
 #include <tuple>
 #include <algorithm>
+#include <thread>
 
 #define _USE_MATH_DEFINES
 #include <math.h>
 
 #include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
 #include "rover_interfaces/srv/send_nav_target.hpp"
 #include "rover_interfaces/msg/geo_pose2_d.hpp"
+#include "rover_interfaces/action/navigate_to_position.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 #include "angles/angles.h"
 
@@ -18,7 +21,9 @@ using namespace GeographicLib;
 using namespace std::chrono_literals;
 using namespace std::placeholders;
 
-const double LINEAR_P = 0.5;
+using NavigateToPosition = rover_interfaces::action::NavigateToPosition;
+
+const double LINEAR_P = 1.0;
 const double ANGULAR_P = 1.0;
 const double DISTANCE_THRESHOLD = 1e-3;
 const double MAX_ANGULAR_VELOCITY = M_PI/4.0;
@@ -62,8 +67,13 @@ std::tuple<double, double> calc_error(rover_interfaces::msg::GeoPose2D from, rov
 class Rover : public rclcpp::Node {
 public:
   Rover() : Node("rover") {
-    nav_target_service = this->create_service<rover_interfaces::srv::SendNavTarget>(
-        "send_nav_target", std::bind(&Rover::nav_target_cb, this, _1, _2));
+    // nav_target_service = this->create_service<rover_interfaces::srv::SendNavTarget>(
+    //     "send_nav_target", std::bind(&Rover::nav_target_cb, this, _1, _2));
+    navigate_to_pose_server = rclcpp_action::create_server<NavigateToPosition>(
+        this, "/navigate_to_position", std::bind(&Rover::handle_goal, this, _1, _2),
+        std::bind(&Rover::handle_cancel, this, _1),
+        std::bind(&Rover::handle_accepted, this, _1)
+    );
 
     pose_listener =
         this->create_subscription<rover_interfaces::msg::GeoPose2D>(
@@ -74,23 +84,42 @@ public:
   }
 
 private:
-  void nav_target_cb(
-      const std::shared_ptr<rover_interfaces::srv::SendNavTarget::Request> request,
-      std::shared_ptr<rover_interfaces::srv::SendNavTarget::Response> response) {
-    RCLCPP_INFO(this->get_logger(), "Received target pose: %f, %f",
-                request->target.position.latitude,
-                request->target.position.longitude);
-    target_pose = request->target;
+  rclcpp_action::GoalResponse handle_goal(const rclcpp_action::GoalUUID &uuid,
+                   std::shared_ptr<const NavigateToPosition::Goal> goal) {
+    RCLCPP_INFO(this->get_logger(), "Received navigation target: %f, %f",
+                goal->target.position.latitude,
+                goal->target.position.longitude);
+    return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+  }
+
+  rclcpp_action::CancelResponse handle_cancel(const std::shared_ptr<rclcpp_action::ServerGoalHandle<NavigateToPosition>> goal_handle) {
+    std::scoped_lock lock(mtx);
+    RCLCPP_INFO(this->get_logger(), "Received cancel request");
+    goal_handle->canceled({});
+    target_pose.reset();
+    active_goal_handle.reset();
+    return rclcpp_action::CancelResponse::ACCEPT;
+  }
+
+  void handle_accepted(const std::shared_ptr<rclcpp_action::ServerGoalHandle<NavigateToPosition>> goal_handle) {
+    std::scoped_lock lock(mtx);
+    active_goal_handle = goal_handle;
+    target_pose = active_goal_handle->get_goal()->target;
   }
 
   void pose_cb(const rover_interfaces::msg::GeoPose2D &msg) {
-    // RCLCPP_INFO_THROTTLE(this->get_logger(), *this->get_clock(), 500, "Received current pose: (%f, %f), %f",
-    //             msg.position.latitude, msg.position.longitude, msg.heading);
+    RCLCPP_DEBUG_THROTTLE(this->get_logger(), *this->get_clock(), 500, "Received current pose: (%f, %f), %f",
+                msg.position.latitude, msg.position.longitude, msg.heading);
     current_pose = msg;
   }
 
   geometry_msgs::msg::Twist calc_cmd_vel() {
     geometry_msgs::msg::Twist ret;
+    std::scoped_lock lock(mtx);
+    if (!target_pose) {
+      return ret;
+    }
+    auto target_pose = this->target_pose.value();
     auto [heading_error, distance] = calc_error(current_pose, target_pose);
     RCLCPP_INFO_THROTTLE(this->get_logger(), *this->get_clock(), 500, "Heading error: %f, Distance error: %f", heading_error, distance);
 
@@ -112,15 +141,29 @@ private:
 
   void timer_cb() {
     auto cmd_vel = calc_cmd_vel();
+
+    if (cmd_vel.linear.x == 0 && cmd_vel.angular.z == 0) {
+      std::scoped_lock lock(mtx);
+      if (auto goal_handle = active_goal_handle) {
+        RCLCPP_INFO(this->get_logger(), "Setting goal as succeeded");
+        goal_handle->succeed(std::make_shared<NavigateToPosition::Result>());
+        RCLCPP_INFO(this->get_logger(), "Set succeeded");
+        active_goal_handle.reset();
+        RCLCPP_INFO(this->get_logger(), "Clearing active goal handle");
+      }
+    }
+
     RCLCPP_INFO(this->get_logger(), "Linear velocity: %f, Angular velocity: %f", cmd_vel.linear.x, cmd_vel.angular.z);
     velocity_publisher->publish(cmd_vel);
   }
 
-  rclcpp::Service<rover_interfaces::srv::SendNavTarget>::SharedPtr nav_target_service;
+  rclcpp_action::Server<NavigateToPosition>::SharedPtr navigate_to_pose_server;
+  std::shared_ptr<rclcpp_action::ServerGoalHandle<NavigateToPosition>> active_goal_handle;
+  std::mutex mtx;
+  std::optional<rover_interfaces::msg::GeoPose2D> target_pose;
+
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr velocity_publisher;
   rclcpp::Subscription<rover_interfaces::msg::GeoPose2D>::SharedPtr pose_listener;
-  rclcpp::Publisher<rover_interfaces::msg::GeoPose2D>::SharedPtr target_publisher;
-  rover_interfaces::msg::GeoPose2D target_pose;
   rover_interfaces::msg::GeoPose2D current_pose;
   rclcpp::TimerBase::SharedPtr runner;
 
